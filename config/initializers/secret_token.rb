142e15bd6684de41839243dae8e7158db3be0ae7
Ali Raza <dejavu87@gmail.com>
2011-10-14 20:33:57 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BidApp::Application.config.secret_token = '8d56628c6286c33eee5322999f13d905a438d47aa3d1797ca1ddf2a0b455090274dc7954a0a70fc5d261d70643e9fbbadc6d62a6d0e35da71292e04a0c359083'
